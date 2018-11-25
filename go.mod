8cb490af49cdda292156491834bcc5997307da21
David Thorpe <djt@mutablelogic.com>
2018-11-25 21:26:48 +0000

    Updated pwm

--- go.mod
+++ go.mod
@@ -1,22 +1,3 @@
 module github.com/djthorpe/gopi-hw
 
-require (
-	cloud.google.com/go v0.33.1 // indirect
-	github.com/djthorpe/gopi v1.0.27
-	github.com/djthorpe/gopi-rpc v1.0.3
-	github.com/golang/lint v0.0.0-20181026193005-c67002cb31c3 // indirect
-	github.com/golang/protobuf v1.2.0
-	github.com/mattn/go-runewidth v0.0.3 // indirect
-	github.com/miekg/dns v1.0.15 // indirect
-	github.com/olekukonko/tablewriter v0.0.1
-	golang.org/x/lint v0.0.0-20181026193005-c67002cb31c3 // indirect
-	golang.org/x/net v0.0.0-20181114220301-adae6a3d119a
-	golang.org/x/oauth2 v0.0.0-20181120190819-8f65e3013eba // indirect
-	golang.org/x/sync v0.0.0-20181108010431-42b317875d0f // indirect
-	golang.org/x/sys v0.0.0-20181122145206-62eef0e2fa9b // indirect
-	golang.org/x/tools v0.0.0-20181122213734-04b5d21e00f1 // indirect
-	google.golang.org/appengine v1.3.0 // indirect
-	google.golang.org/genproto v0.0.0-20181109154231-b5d43981345b // indirect
-	google.golang.org/grpc v1.16.0
-	honnef.co/go/tools v0.0.0-20180920025451-e3ad64cb4ed3 // indirect
-)
+require github.com/djthorpe/gopi v1.0.27
--- sys/pwm/pi-blaster.go
+++ sys/pwm/pi-blaster.go
@@ -10,8 +10,15 @@
 package pwm
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"os"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
 
 	// Frameworks
 	"github.com/djthorpe/gopi"
@@ -26,10 +33,18 @@ type PiBlaster struct {
 }
 
 type piblaster struct {
-	fifo string
-	log  gopi.Logger
-	fh   *os.File
+	fifo       string
+	log        gopi.Logger
+	fh         *os.File
+	period     time.Duration
+	samples    uint
+	min_period time.Duration
+	max_period time.Duration
 }
+
+var (
+	regexpFrequency = regexp.MustCompile("^([0-9]+\\.?[0-9]*)\\s*(hz|khz|mhz)$")
+)
 
 ////////////////////////////////////////////////////////////////////////////////
 // OPEN AND CLOSE
@@ -48,12 +63,21 @@ func (config PiBlaster) Open(log gopi.Logger) (gopi.Driver, error) {
 	// Open FIFO
 	if stat, err := os.Stat(this.fifo); os.IsNotExist(err) == true {
 		return nil, err
-	} else if stat.Mode() != os.ModeNamedPipe {
-		return nil, fmt.Errorf("Not a named pipe: %v", this.fifo)
+	} else if stat.Mode().IsDir() {
+		return nil, fmt.Errorf("Not a file: %v", this.fifo)
 	} else if fh, err := os.OpenFile(this.fifo, os.O_WRONLY|os.O_APPEND, 0755); err != nil {
 		return nil, err
 	} else {
 		this.fh = fh
+	}
+
+	// Read parameters
+	if _, err := os.Stat(config.Exec); os.IsNotExist(err) == true {
+		return nil, err
+	} else if output, _ := exec.Command(config.Exec, "-D").CombinedOutput(); len(output) == 0 {
+		return nil, fmt.Errorf("No output: %v", config.Exec)
+	} else if err := this.set_values_from_output(output); err != nil {
+		return nil, err
 	}
 
 	// success
@@ -65,6 +89,99 @@ func (this *piblaster) Close() error {
 	this.log.Debug("<sys.hw.PWM.PiBlaster>Close{ fifo='%v' }", this.fifo)
 
 	// Release resources
+	if err := this.fh.Close(); err != nil {
+		return err
+	}
 
 	return nil
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// STRINGIFY
+
+func (this *piblaster) String() string {
+	return fmt.Sprintf("<sys.hw.PWM.PiBlaster>{ fifo='%v' period=%v min_period=%v max_period=%v samples=%v }", this.fifo, this.period,this.min_period,this.max_period,this.samples)
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// IMPLEMENTATION
+
+func (this *piblaster) Frequency(gopi.GPIOPin) (float32, error) {
+	return 0, gopi.ErrNotImplemented
+}
+
+func (this *piblaster) SetFrequency(float32, gopi.GPIOPin) error {
+	return gopi.ErrNotImplemented
+}
+
+func (this *piblaster) DutyCycle(gopi.GPIOPin) (float32, error) {
+	return 0, gopi.ErrNotImplemented
+}
+
+func (this *piblaster) SetDutyCycle(float32, gopi.GPIOPin) error {
+	return gopi.ErrNotImplemented
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// PRIVATE METHODS
+
+func (this *piblaster) set_values_from_output(output []byte) error {
+	scanner := bufio.NewScanner(bytes.NewReader(output))
+	for scanner.Scan() {
+		if tuple := strings.Split(scanner.Text(), ":"); len(tuple) != 2 {
+			continue
+		} else if err := this.set_value(strings.ToLower(strings.TrimSpace(tuple[0])), strings.TrimSpace(tuple[1])); err != nil {
+			return err
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (this *piblaster) set_value(name, value string) error {
+	this.log.Debug2("<sys.hw.PWM.PiBlaster>set_value{ name='%v' value='%v' }", name, value)
+	switch {
+	case strings.HasSuffix(name, "frequency"):
+		if period, err := ParseFrequency(value); err != nil {
+			return err
+		} else {
+			this.period = period
+		}
+	case strings.HasSuffix(name, "steps"):
+		if samples, err := strconv.ParseUint(value,10, 64); err != nil {
+			return err
+		} else {
+			this.samples = uint(samples)
+		}
+	case strings.HasPrefix(name, "maximum period"):
+		if max, err := time.ParseDuration(value); err != nil {
+			return err
+		} else {
+			this.max_period = max
+		}
+	case strings.HasPrefix(name, "minimum period"):
+		if min, err := time.ParseDuration(value); err != nil {
+			return err
+		} else {
+			this.min_period = min
+		}
+	}
+	return nil
+}
+
+func ParseFrequency(value string) (time.Duration, error) {
+	if parts := regexpFrequency.FindStringSubmatch(strings.ToLower(value)); len(parts) != 3 {
+		return 0, gopi.ErrBadParameter
+	} else if num, err := strconv.ParseFloat(parts[1], 64); err != nil {
+		return 0, err
+	} else {
+		switch parts[2] {
+		case "hz":
+			return time.Nanosecond * time.Duration((1E9 / num)), nil
+		default:
+			return 0, gopi.ErrBadParameter
+		}
+	}
 }
